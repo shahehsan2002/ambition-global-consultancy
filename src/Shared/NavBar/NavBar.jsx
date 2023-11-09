3a2269388f388dd84c43224808302afcafa27496
shahehsan2002 <shahehsan2002@.com>
2023-11-10 00:39:41 +0600

    create simple top banner react responsive carousel

--- src/Shared/NavBar/NavBar.jsx
+++ src/Shared/NavBar/NavBar.jsx
@@ -3,18 +3,18 @@
 const NavBar = () => {
     const navOptions =<>
      <li><a>Item 1</a></li>
-        <li>
+        {/* <li>
           <a>Parent</a>
           <ul className="p-2">
             <li><a>Submenu 1</a></li>
             <li><a>Submenu 2</a></li>
           </ul>
-        </li>
+        </li> */}
         <li><a>Item 3</a></li>
     </>
     return (
         <>
-            <div className="navbar bg-base-100">
+            <div className="navbar fixed z-10 bg-opacity-30 bg-black text-white max-w-screen-xl ">
   <div className="navbar-start">
     <div className="dropdown">
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
