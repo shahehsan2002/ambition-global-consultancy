999ed5e1e31b8bbfc805d9e019bf8fe146ed734b
shahehsan2002 <shahehsan2002@.com>
2023-11-10 19:00:32 +0600

    Shared section title for different sections

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,8 +15,10 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    "react/prop-types": "off",
     'react-refresh/only-export-components': [
       'warn',
+      
       { allowConstantExport: true },
     ],
   },
