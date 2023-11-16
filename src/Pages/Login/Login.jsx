1450e4ab20846254158c858ce2eedc3cf3c9eaf9
shahehsan2002 <shahehsan2002@.com>
2023-11-16 19:09:29 +0600

    Use Regex Validation In React Hook Form And SignUp

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState, useContext } from 'react';
 import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
 import { AuthContext } from '../../providers/AuthProviders';
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
 
 
 const Login = () => {
@@ -38,7 +39,11 @@ const Login = () => {
     };
 
     return (
-        <div className="hero min-h-screen bg-base-200">
+      <>
+        <Helmet>
+                <title>Bistro Boss |Login</title>
+            </Helmet>
+       <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col md:flex-row-reverse">
                 <div className="text-center md:w-1/2 lg:text-left">
                     <h1 className="text-5xl font-bold">Login now!</h1>
@@ -96,6 +101,7 @@ const Login = () => {
                 </div>
             </div>
         </div>
+      </> 
     );
 };
 
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,83 +1,96 @@
 
+import { useContext } from 'react';
+import { Helmet } from 'react-helmet-async';
 import { useForm } from 'react-hook-form';
+import { AuthContext } from '../../providers/AuthProviders';
 
 const SignUp = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
 
-  const onSubmit = (data) => {
-    console.log(data);
-  };
+    const {createUser}= useContext(AuthContext);
 
-  return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Sign up now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In
-            deleniti eaque aut repudiandae et a id nisi.
-          </p>
-        </div>
-        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
-            <div className="form-control">
-              <label htmlFor="name" className="label">
-                <span className="label-text">Name</span>
-              </label>
-              <input
-                type="text"
-                id="name"
-                {...register('name', { required: 'Name is required' })}
-                placeholder="Name"
-                className={`input input-bordered ${errors.name ? 'input-error' : ''}`}
-              />
-              {errors.name && <span className="text-xs text-error">{errors.name.message}</span>}
+    const onSubmit = (data) => {
+        console.log(data);
+        createUser(data.email,data.password)
+        .then(result => {
+            const loggedUser = result.user;
+            console.log(loggedUser);
+        })
+    };
+
+    return (
+        <>
+         <Helmet>
+                <title>Bistro Boss |Sign Up</title>
+            </Helmet>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">Sign up now!</h1>
+                        <p className="py-6">
+                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In
+                            deleniti eaque aut repudiandae et a id nisi.
+                        </p>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                            <div className="form-control">
+                                <label htmlFor="name" className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input
+                                    type="text"
+                                    id="name"
+                                    {...register('name', { required: 'Name is required' })}
+                                    placeholder="Name"
+                                    className={`input input-bordered ${errors.name ? 'input-error' : ''}`}
+                                />
+                                {errors.name && <span className="text-xs text-error">{errors.name.message}</span>}
+                            </div>
+                            <div className="form-control">
+                                <label htmlFor="email" className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    {...register('email', { required: 'Email is required', pattern: /^\S+@\S+$/i })}
+                                    placeholder="Email"
+                                    className={`input input-bordered ${errors.email ? 'input-error' : ''}`}
+                                />
+                                {errors.email && <span className="text-xs text-error">{errors.email.message}</span>}
+                            </div>
+                            <div className="form-control">
+                                <label htmlFor="password" className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input
+                                    type="password"
+                                    id="password"
+                                    {...register('password', { required: 'Password is required', minLength: 6 })}
+                                    placeholder="Password"
+                                    className={`input input-bordered ${errors.password ? 'input-error' : ''}`}
+                                />
+                                {errors.password && <span className="text-xs text-error">{errors.password.message}</span>}
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">
+                                        Forgot password?
+                                    </a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <input className='btn btn-primary' type="submit" value="SignUp" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <div className="form-control">
-              <label htmlFor="email" className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                id="email"
-                {...register('email', { required: 'Email is required', pattern: /^\S+@\S+$/i })}
-                placeholder="Email"
-                className={`input input-bordered ${errors.email ? 'input-error' : ''}`}
-              />
-              {errors.email && <span className="text-xs text-error">{errors.email.message}</span>}
-            </div>
-            <div className="form-control">
-              <label htmlFor="password" className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="password"
-                id="password"
-                {...register('password', { required: 'Password is required', minLength: 6 })}
-                placeholder="Password"
-                className={`input input-bordered ${errors.password ? 'input-error' : ''}`}
-              />
-              {errors.password && <span className="text-xs text-error">{errors.password.message}</span>}
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
-            </div>
-            <div className="form-control mt-6">
-              <button type="submit" className="btn btn-primary">
-                Sign Up
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+        </>
+    );
 };
 
 export default SignUp;
